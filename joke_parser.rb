adffb8bd0b6c83f926d56741bb418a24bddbd9af
Casey Ann Cumbow <cumbow8@gmail.com>
2017-04-25 14:27:28 -0400

    Add another file and parser method

--- joke_parser.rb
+++ joke_parser.rb
@@ -1,6 +1,18 @@
 require 'nokogiri'
-doc = Nokogiri::HTML(File.open('laffy_taffy.html'))
 
-doc.search('ol > li').map do |element|
-  puts element.inner_text.strip
+def laffy_taffy_parser
+  doc = Nokogiri::HTML(File.open('laffy_taffy.html'))
+
+  doc.search('ol > li').map do |element|
+    puts element.inner_text.strip
+  end
 end
+
+def chartcons_parser
+  doc = Nokogiri::HTML(File.open('chartcons-jokes.html'))
+  doc.search('.wp-content > p').map do |element|
+    puts element.inner_text.strip
+  end
+end
+
+chartcons_parser
