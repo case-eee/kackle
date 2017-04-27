496afd3aa346ec37969ebbe9ef57b40ca739e288
Sal Espinosa <sespinos@gmail.com>
2017-04-27 15:00:35 -0600

    Create migration to add 'active' to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,23 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170425222849) do
+ActiveRecord::Schema.define(version: 20170427201357) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
+  create_table "jokes", force: :cascade do |t|
+    t.string   "question"
+    t.string   "answer"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
-    t.string "email"
-    t.string "password_digest"
-    t.string "phone"
+    t.string  "email"
+    t.string  "password_digest"
+    t.string  "phone"
+    t.boolean "active",          default: true
   end
 
 end
