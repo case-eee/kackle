2d89baf1f4c0375c33c86279abc3a9c425ed4691
Casey Ann Cumbow <cumbow8@gmail.com>
2017-05-09 11:05:56 -0400

    Add .active? to User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ApplicationRecord
   has_secure_password
   validates :email, presence: true
   validates :phone, presence: true
+
+  scope :active?, -> { where(active: true) }
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,13 @@ describe User do
   it {should validate_presence_of(:email)}
   it {should validate_presence_of(:phone)}
   it {should have_secure_password}
+
+  describe ".active?" do
+    it "returns all the active users" do
+      user1, user2 = create_list(:user, 2)
+      user3 = create(:user, active: false)
+
+      expect(User.active?.count).to eq(2)
+    end
+  end
 end
